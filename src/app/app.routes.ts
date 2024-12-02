218b9ebfe3f97064813a740293612fe759fb81a5
Federico <fedealtava1992@gmail.com>
2024-12-02 19:59:29 +0100

    favourite component added and new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddComponent } from './add/add.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'favourites',component:FavouritesComponent},
     {path:'add',component:AddComponent},
-    {path:'**',component:HomeComponent}
+    {path:'**',component:HomeComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    
 ];
